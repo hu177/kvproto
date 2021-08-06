4d5b92071b14dd9b52502bf61dafdacbfe251aa5
hurui <hurui@bilibili.com>
2021-08-06 11:00:19 +0800

    提交关于hdfs的功能更改

--- pkg/backup/hdfs.go
+++ pkg/backup/hdfs.go
@@ -1,1 +1,19 @@
 package backup
+
+type HDFS struct {
+
+}
+
+type StorageBackend_HDFS struct {
+	HDFS *HDFS
+	Local *Local
+}
+
+func (*StorageBackend_HDFS) isStorageBackend_Backend()         {}
+
+func (*StorageBackend_HDFS) MarshalTo([]byte) (int, error) {
+	return 0,nil
+}
+func (*StorageBackend_HDFS) Size() int {
+	return 0
+}
